c0f436cac0e03bfe0062223bb9640faeac654585
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-03 02:43:04 +0300

    First Trial

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 class MyString:
-  pass
+    
+    def __init__(self, value = ""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+    
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value[-1] == "."
+    
+    def is_question(self):
+        return self._value[-1] == "?"
+    
+    def is_exclamation(self):
+        return self._value[-1] == "!"
+    
+    def count_sentences(self):
+        number_of_sentences = []
+        pwd = self._value
+        pwd = pwd.replace(" ", "")
+        pwd = pwd.replace("!!", "!")
+        pwd = pwd.replace("...", ".")
+        
+        for char in pwd:
+            if char in (".", "!", "?", "...", "!!"):
+                a = pwd[0 : pwd.index(char) + 1]
+                pwd = pwd.replace(a,"")
+                number_of_sentences.append(a)
+        return len(number_of_sentences)
